fc29cd38f1ee9cea6b071119f76c5cd10fcea372
Elane Souza <104568689+ElaneSz@users.noreply.github.com>
2023-08-07 22:51:10 +0000

    Login personalizado

--- client/src/templetes/Login.tsx
+++ client/src/templetes/Login.tsx
@@ -3,34 +3,73 @@ import { styled } from "styled-components"
 const Container = styled.div`
     display: flex;
     height: 100vh;
+    
 
     > * {
-        flex-grow: 1;
+        flex-grow: 1
     }
 
     .image {
-        background-color: black;
+        background-color:  #9788c4;
+        color: #FFFFFF;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        .medusa {
+            height: 300px;
+            width: 300px;
+        }
     }
 
     .container-login-form {
         display: flex;
         flex-direction: column;
-        aling-itens: center;
-    }
+        justify-content: center;
+        align-items: center;
 
-    form {
-        border: 1px solid black;
-        panding: 2rem;
+        form {
+            font-size: 1.5rem;
+            padding: 2rem;
+            box-shadow: 0 0 7em rgb(113, 60, 156);
+            border-radius: 30px 0;
 
-        div {
-            margin-botton: 1rem;
+            div {
+                margin-bottom: 1rem;
 
-            label {
-                display: block;
+                label {
+                    display: block;
+                    color: #5b5b5b;
+                }
+
+                input {
+                    font-size: 1.5rem;
+                    padding: .5rem;
+                    border: 0 none;
+                    border-bottom: 1px solid black;
+                    outline: none;
+                }
             }
 
-            input {
-                border: 0 none;
+            button {
+                font-size: .8em;
+                background: #59429d;
+                border: 0;
+                border-radius: 5px;
+                margin-bottom: .3rem;
+                color: #ffffff;
+                padding: 0.2em 0.6em;
+                box-shadow: 2px 2px 2px rgba(0,0,0,0.2);
+                text-shadow: 1px 1px 1px rgba(0,0,0,0.5);
+                transform: translate(-3%, -3%);
+                cursor: pointer;
+                margin-right: 10px;
+            }
+
+            button:hover {
+                background: #afa1db;
+                box-shadow: inset 2px 2px 2px rgba(0,0,0,0.2);
+                text-shadow: none;
             }
         }
     }
@@ -39,6 +78,7 @@ const Container = styled.div`
 export default () => <>
     <Container>
         <div className="image">
+            <img className="medusa" src="src/components/medusa.png" alt="Medusa" />
         </div>
         <div className="container-login-form">
             <form>
